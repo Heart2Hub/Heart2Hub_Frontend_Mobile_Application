692a293fa7baf3d28311347174a2c417d8f26592
kohwenjie <70895902+kohwenjie@users.noreply.github.com>
2023-09-05 11:13:51 +0800

    initial setup
    boilerplate code from running the ionic start

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'heart2hub-mobile-application',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
