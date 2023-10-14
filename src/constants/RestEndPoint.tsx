c547e95d334748a9d8e4ac851ebc0437ba22c929
Elgin Chan <elginchan13@gmail.com>
2023-10-14 18:59:29 +0800

    SR2

--- src/constants/RestEndPoint.tsx
+++ src/constants/RestEndPoint.tsx
@@ -1,4 +1,4 @@
-const IP_CONFIG = ""; // Paste your IP Address here
+const IP_CONFIG = "http://localhost"; // Replace localhost with your IP Address
 
 export const REST_ENDPOINT = IP_CONFIG + ":8080";
 export const IMAGE_SERVER = IP_CONFIG + ":3001";
