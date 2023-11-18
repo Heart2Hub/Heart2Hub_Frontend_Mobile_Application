7708bc47f3e801a00445aee42adaa62aa9f8775c
Elgin Chan <elginchan13@gmail.com>
2023-11-18 13:52:41 +0800

    change IP config

--- src/constants/RestEndPoint.tsx
+++ src/constants/RestEndPoint.tsx
@@ -1,4 +1,4 @@
-const IP_CONFIG = "http://localhost"; // Replace localhost with your IP Address
+const IP_CONFIG = "http://192.168.1.118"; // Replace localhost with your IP Address
 
 export const REST_ENDPOINT = IP_CONFIG + ":8080";
 export const IMAGE_SERVER = IP_CONFIG + ":3001";
